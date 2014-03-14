22237152f1c29759c66ef63ccf4c64c4f7cb73e1
Jim Town <james.ross.town@gmail.com>
2014-03-13 20:48:54 -0400

    updated images and geo-stl-gui.py to version 1.1

--- geo-stl-gui.py
+++ geo-stl-gui.py
@@ -1,4 +1,3 @@
-#from pylab import imread
 from scipy.ndimage import gaussian_filter
 from stl_tools import numpy2stl
 import Image
@@ -9,15 +8,16 @@ import sys
 
 # geomapapp -> find map -> grid dialog -> black to white -> no sun -> preferences -> turn off axis -> save as .png
 
-BGCOLOR='#E0EEEE'
-imageScale=1  #minimum image size divided by (ie 2 means half)
+BGCOLOR='#E5EDF3' #'#E0EEEE'
+minImageScale=1  #minimum image size divided by (ie 2 means half)
 maxDimension=400 #much larger than this and the time it takes to compile is too long, the file is too big, and the model is much larger than a printer can print
-border=6 #gets rid of black lat and long lines in the .png
+border=6 #gets rid of black lat and long lines in the .png default=6
 bottom=True #true prints a bottom
-minThickPct=.03
+minThickPct=.03 #percent of total height to put at bottom
 maxWidth=10000#140 #10000, #afinia website says it can print a 5.5" cube 140mm
 maxDepth=10000#140 #10000, #10000mm=10m user can shrink it with their printer software
 maxHeight=10000#140 #10000, #
+fileTypes=['png','tif','iff']
 
 
 
@@ -40,11 +40,6 @@ class Application(Tkinter.Frame):
         img = Image.open(self.fileLocation)
         self.width,self.height=img.size
         self.imageScale=max(self.imageScale,((max(self.width, self.height))/maxDimension))
-        print self.imageScale
-##        if self.fileType=='png':
-##            self.imageScale=1
-##        elif self.fileType=='tif' or self.fileType=='iff': #DEM Tiffs seem to be really really big
-##            self.imageScale=8
         imgCrop=img.crop((border,border,self.width-border,self.height-border))
         imgSmall = imgCrop.resize((self.width/self.imageScale,self.height/self.imageScale), Image.ANTIALIAS)
         #other options: ANTIALIAS, NEAREST, BILINEAR, BICUBIC
@@ -74,15 +69,23 @@ class Application(Tkinter.Frame):
         self.fileType=self.fileLocation[-3:].lower()
 
     def main(self):
-        while self.high.get()<=self.low.get() or not self.fileLocation:
-            self.outputBox.delete(1.0,Tkinter.END)
-            if self.high.get()<=self.low.get():
-                output='High Point must be greater than Low Point.'
-            else:
-                output='Please choose a file.'
+        self.outputBox.delete(1.0,Tkinter.END)
+        try:
+            area=float(self.area.get())
+        except ValueError:
+            output=self.area.get()+' is not a number, try again.'
             self.outputBox.insert(Tkinter.END,output)
             return
-        self.outputBox.delete(1.0,Tkinter.END)
+        else:
+            while self.high.get()<=self.low.get() or area<=0 or not self.fileLocation or self.fileType not in fileTypes:# or area>=100000:
+                if self.high.get()<=self.low.get():
+                    output='High Point must be greater than Low Point.'
+                elif area<=0:#not self.fileLocation:
+                    output='Please enter a valid area.'# between 0 and 100,000 square kilometers.'
+                else:
+                    output='Please choose a .png file.'
+                self.outputBox.insert(Tkinter.END,output)
+                return
         self.outputBox.insert(Tkinter.END,'Working... ')#This may take a while.')
         root.update()
         #start redirecting stdout:
@@ -94,13 +97,13 @@ class Application(Tkinter.Frame):
         #pixels translate to millimeters 
         sqrtAreaMM=(self.height*self.width)**.5
         #user entered area in square kilometers converted to meters
-        sqrtAreaM=(self.area.get()*1000000)**.5
+        sqrtAreaM=(float(self.area.get())*1000000)**.5
         #adjusts the height of the model based on the difference in heights
         scaleFactor=self.vertExag.get()*(sqrtAreaMM/sqrtAreaM)*heightDiffMeters/(heightDiffColors*self.imageScale)
             
         numpy2stl (heightArray, fileName+".stl",
                    scale=scaleFactor, #maximum height?
-                   #mask_val=1, #cuts of bottom to make islands
+                   #mask_val=1, #cuts off bottom to make islands
                    max_width=maxWidth,
                    max_depth=maxDepth,
                    max_height=maxHeight,
@@ -108,64 +111,99 @@ class Application(Tkinter.Frame):
                    solid=bottom) #True means it will make a bottom
         self.outputBox.delete(1.0,Tkinter.END)
         self.outputBox.insert(Tkinter.END,'Program is finished.')
-        # To stop redirecting stdout:
+        #stop redirecting stdout:
         sys.stdout = sys.__stdout__
-        
 
     def makeSlider(self, caption, default, width=None, **options):
-        Tkinter.Label(root, text=caption, bg='#E0EEEE').grid(row=3, column=self.column, columnspan=2, sticky='w')
-        slider = Tkinter.Scale(root, **options)
+        Tkinter.Label(self.frame, text=caption, bg=BGCOLOR).grid(row=3, column=self.column, columnspan=2, sticky='w')
+        slider = Tkinter.Scale(self.frame, **options)
         slider.grid(row=4, column=self.column, columnspan=2)
         self.column+=2
         slider.set(default)
         return slider
 
-    def areaSlider(self, caption, default, width=None, **options):
-        Tkinter.Label(root, text=caption, bg='#E0EEEE').grid(row=5, column=1)
-        slider = Tkinter.Scale(root, **options)
-        slider.grid(row=5, column=2, columnspan=6)
-        slider.set(default)
-        return slider
+    def intInput(self, caption, width=None, **options):
+        Tkinter.Label(self.frame, text=caption, bg=BGCOLOR).grid(row=5, column=1, sticky='e')
+        entry = Tkinter.Entry(self.frame, **options)
+        entry.grid(row=5, column=2, columnspan=3, sticky='ew')
+        entry.insert(Tkinter.END,'1')
+        return entry
 
     def makeButton(self, **options):
-        button = Tkinter.Button(root, **options)
+        button = Tkinter.Button(self.frame, **options)
         button.grid(row=3, column=self.column, rowspan=2)
         self.column+=1
         return button
+    
+    def makeScrollBar(self):
+        winHeight=min(1060,root.winfo_screenheight()-500)
+        scrollbar = Tkinter.Scrollbar(self, orient="vertical")
+        scrollbar.pack(side='right',fill='y', expand=False)
+        self.canvas= Tkinter.Canvas(self,bd=0, #width=935, height=winHeight,
+                               borderwidth=0, background=BGCOLOR,
+                               highlightthickness=0,
+                               yscrollcommand=scrollbar.set)
+        self.canvas.pack(side='left', fill='both', expand=True)
+        scrollbar.config(command=self.canvas.yview)
 
+        self.canvas.xview_moveto(0)
+        self.canvas.yview_moveto(0)
 
+        self.frame = Tkinter.Frame(self.canvas, background=BGCOLOR)
+        newWindow = self.canvas.create_window(0,0,window=self.frame,anchor='nw')
+
+        def configureFrame(event):
+            size=(self.frame.winfo_reqwidth(), self.frame.winfo_reqheight())
+            self.canvas.config(scrollregion="0 0 %s %s" % size)
+            if self.frame.winfo_reqwidth() != self.canvas.winfo_width():
+                self.canvas.config(width=self.frame.winfo_reqwidth())
+        self.frame.bind('<Configure>', configureFrame)
+        
+        def configureCanvas(event):
+            if self.frame.winfo_reqwidth() != self.canvas.winfo_width():
+                self.canvas.config(width=self.frame.winfo_width())
+        self.canvas.bind('<Configure>', configureCanvas)
+        self.pack()
+
+    def shrinkWindow(self, event): #when the window shrinks the scrollbar/canvas shrinks with it
+        if root.winfo_height()!=self.canvas.winfo_height():
+            self.canvas.config(height=root.winfo_height())        
+    
     def insertImage(self,filename,**options):
         theImage=Tkinter.PhotoImage(file="images/"+filename+".gif")
-        instructionLabel = Tkinter.Label(root, image=theImage)
+        instructionLabel = Tkinter.Label(self.frame, image=theImage)
         instructionLabel.photo=theImage
         instructionLabel.grid(**options)
 
     def __init__(self, master=None):
-        Tkinter.Frame.__init__(self, master)
+        frame=Tkinter.Frame.__init__(self, master)
+        root.bind('<Configure>', self.shrinkWindow)
+        self.makeScrollBar()
         self.fileLocation = None
         self.width=0
         self.height=0
         self.fileType='png'
         self.grid()
-        self.imageScale=imageScale #image size divided by (ie 2 means half): default is 1
+        self.imageScale=minImageScale #image size divided by (ie 2 means half): default is 1
         self.column=1
         self.insertImage('logo',row=0,column=0,rowspan=10)
         self.high=self.makeSlider("Highest Point (m)", 100, from_=0, to=10000, length=200, bg=BGCOLOR, resolution=10, orient=Tkinter.HORIZONTAL)
         self.low =self.makeSlider("Lowest Point (m)", 0, from_=0, to=10000, length=200, bg=BGCOLOR, resolution=10, orient=Tkinter.HORIZONTAL)
-        self.area = self.areaSlider("Area (sq km)",50,from_=1, to=100000, length=600, bg=BGCOLOR, resolution=1, orient=Tkinter.HORIZONTAL) 
+        self.area = self.intInput("Enter Area (sq km):",bg=BGCOLOR,width=50)
+#        self.area = self.areaSlider("Area (sq km)",50,from_=1, to=100000, length=600, bg=BGCOLOR, resolution=1, orient=Tkinter.HORIZONTAL) 
         self.vertExag=self.makeSlider("Vertical Exaggeration", 1, from_=.1, to=10, length=150, bg=BGCOLOR, resolution=.1, orient=Tkinter.HORIZONTAL)
         self.fileButton=self.makeButton(text="Choose File", width=15,command=self.pickFile)
         self.startButton=self.makeButton(text="Start", width=10,command=self.main)
-        self.outputBox = Tkinter.Text(root, bg=BGCOLOR, height= 1, fg='black', font=("Helvetica", 10), relief=Tkinter.FLAT, yscrollcommand='TRUE')#fg='black', relief=Tkinter.SUNKEN, 
-        self.outputBox.grid(row=6, column=1, columnspan=6)
-        self.insertImage('instructions',row=7,column=1,columnspan=10)
-
+        self.outputBox = Tkinter.Text(self.frame, bg=BGCOLOR, height= 1, fg='black', font=("Helvetica", 10), relief=Tkinter.FLAT, yscrollcommand='TRUE')#fg='black', relief=Tkinter.SUNKEN, 
+        self.outputBox.grid(row=6, column=1, columnspan=8, sticky='ew')
+        self.insertImage('instructions',row=7,column=1,rowspan=10,columnspan=10)
 
 
 root = Tkinter.Tk()
 root.title("GeoMapApp to *.stl")
-root.configure(background='#E0EEEE')
-root.geometry('930x1035')#('800x150+100+100')
+root.configure(background=BGCOLOR)
+winHeight=min(1060,root.winfo_screenheight()-100)
+root.geometry('950x'+str(winHeight)+'+10+10')
 app = Application(master=root)
 app.mainloop()
 
